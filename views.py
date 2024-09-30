5de5a48405aff371e053e9cb2217b45051b7b2f8
supriyaneela <167035092+supriyaneela@users.noreply.github.com>
2024-09-29 21:36:34 -0500

    Add files via upload

---
+++ views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+import json
+def index(request):
+	data={ 'Message':'Hello World!'}
+	return HttpResponse(json.dumps(data),content_type="application/json")
